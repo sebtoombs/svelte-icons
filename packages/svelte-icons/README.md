130c847afb48c8d1c3b338e92d4ad96a5e0ffc7a
Seb <sebtoombs@gmail.com>
2021-08-09 21:33:25 +1000

    Add ant icons and bootstrap icons

---
+++ packages/svelte-icons/README.md
@@ -0,0 +1,6 @@
+This readme is really just notes right now.
+
+Add new icon lib:
+
+1. git submodule add {repo_url} packages/svelte-icons/icons/{lib_name}
+2. Edit packages/svelte-icons/icons/index.cjs and add
--- packages/svelte-icons/icons/index.cjs
+++ packages/svelte-icons/icons/index.cjs
@@ -11,6 +11,34 @@ module.exports = {
 					formatter: (name) => `Eva${name}`
 				}
 			]
+		},
+		{
+			id: 'ant',
+			name: 'Ant Design Icons',
+			contents: [
+				{
+					files: path.resolve(__dirname, 'ant-icons/packages/icons-svg/svg/filled/*.svg'),
+					formatter: (name) => `AiFill${name}`
+				},
+				{
+					files: path.resolve(__dirname, 'ant-icons/packages/icons-svg/svg/outline/*.svg'),
+					formatter: (name) => `AiOutline${name}`
+				},
+				{
+					files: path.resolve(__dirname, 'ant-icons/packages/icons-svg/svg/twotone/*.svg'),
+					formatter: (name) => `AiTwotone${name}`
+				}
+			]
+		},
+		{
+			id: 'bs',
+			name: 'Bootstrap Icons',
+			contents: [
+				{
+					files: path.resolve(__dirname, 'bootstrap-icons/icons/*.svg'),
+					formatter: (name) => `Bs${name}`
+				}
+			]
 		}
 	]
 };
